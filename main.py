7133a69c6b6c9ab71dbc17e49e22ff3ce76db9eb
Zander Samuel <zander7310@gmail.com>
2024-07-20 14:46:23 -0700

    added some input validation

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from model import Person
+import os
 
-# list of different "people"
 people: dict[str: Person]= dict()
 
 
 def main():
 
     continue_program = True
-    print("== DASHBOARD ==")
+    print("\n" * 100)
     print("Welcome! Here you can chat with different people :D")
 
     while continue_program == True:
+        print("== DASHBOARD ==")
         print(f"Your current people are: {list(people.keys())}")
         print(f"""Your options are:
                 \t* quit - terminate the program
@@ -23,22 +24,35 @@ def main():
             case [action]:
                 if action == "quit":
                     continue_program = False
+                    if os.path.exists("output.jpg"):
+                        os.remove("output.jpg")
+                else:
+                    print("ERROR, command not recognized")
             case [action, name]:
                 if action == "delete":
-                    people.pop(name)
+                    if name in people:
+                        people.pop(name)
+                    else:
+                        print("ERROR, person does not exist")
                 elif action == "chat":
                     if name in people:
                         chat(people[name])
                     else:
                         print("ERROR: person does not exist")
+                else:
+                    print("ERROR, command not recognized")
 
             case [action, *objects]:
                 if action == "create":
+                    if len(objects) < 3:
+                        print("ERROR, not enough arguments supplied")
                     new_person = Person(country=objects[1], name=objects[0], age=objects[2])
                     people[objects[0]] = new_person
 
             case _:
-                print("Command not recognized")
+                print("ERROR, Command not recognized")
+            
+        print("\n" * 4)
 
 
 
@@ -46,35 +60,16 @@ def chat(person: Person):
     
     continue_chat = True
 
-    print(f"== CHAT WITH {str(person).upper()}==")
+    print(f"== CHAT WITH {str(person).upper()} ==")
     print("Type 'exit' to return to dashboard")
     print(person.say_hi())
 
-    user_input = input("Enter: ")
-
-    while user_input != "exit":
-        print(person.respond(user_input.strip()), flush=True)
+    while continue_chat:
         user_input = input("Enter: ")
-    
-
-
-
-# print("Hello welcome to [name TBD], where you can chat with different people!")
-# country = input("Enter the person's country of origin: ")
-# people.append(Person(country=country))
-# print(f"You want to talk with someone from this country {country}")
-
-# # create the model
-
-
-
-# while continue_chat:
-#     # print(model.say_hi())
-#     user_input = input("Enter your response ")
-#     if user_input == "quit":
-#         continue_chat = False
-#     else:
-#         print(model.respond(user_input)) 
+        if user_input == "exit":
+            continue_chat = False
+        else:
+            print(person.respond(user_input))
 
 
 if __name__ == "__main__":
