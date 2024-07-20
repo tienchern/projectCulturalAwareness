6faa1efc0bd8cee85f9408e3e200e2a9c49ff17f
Zander Samuel <zander7310@gmail.com>
2024-07-20 10:58:15 -0700

    basic input loop

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import model
 
 # list of different "people"
 people = []
+continue_chat = True
 
 print("Hello welcome to [name TBD], where you can chat with different people!")
 country = input("Enter the person's country of origin: ")
 print(f"You want to talk with someone from this country {country}")
+
+# create the model
+while continue_chat:
+    print(model.say_hi())
+    user_input = input("Enter your response (type quit to quit): ")
+    if user_input == "quit":
+        continue_chat = False
+    else:
+        print(model.respond(user_input))
---
+++ model.py
@@ -0,0 +1,5 @@
+def say_hi() -> str:
+    return "Hi there!"
+
+def respond(user_input: str) -> str:
+    return f"You said {user_input}" 
