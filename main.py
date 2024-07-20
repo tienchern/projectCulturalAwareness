5e04f922ec4f71d36ffdc6ad63a8423efc361c39
Zander Samuel <zander7310@gmail.com>
2024-07-20 10:50:01 -0700

    adding main :)

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# list of different "people"
+people = []
+
+print("Hello welcome to [name TBD], where you can chat with different people!")
+country = input("Enter the person's country of origin: ")
+print(f"You want to talk with someone from this country {country}")
