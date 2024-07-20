b2835faeb03c52657e69a83edbbce213d8a9134a
TC50001 <57049145+TC5001@users.noreply.github.com>
2024-07-20 12:53:57 -0700

    DARTH VADER HOOOO_SHHHHH

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.embeddings import OctoAIEmbeddings
 from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
 from langchain.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter
+from langchain.vectorstores import FAISS
 
 load_dotenv()
 OCTOAI_API_TOKEN = os.environ["OCTOAI_API_TOKEN"]
@@ -18,6 +20,7 @@ class Person:
     _llm = None
     _template = None
     _prompt = None
+    _retriever = None
 
     def __init__(self, country: str, name: str):
         self.set_parameters(country)
@@ -37,6 +40,8 @@ class Person:
     def set_parameters(self, country: str):
         self._country = country
 
+        self._retriever = get_context(self._country)
+
         self._template = """You are a person called {name} from the different culture of {country}. You are {age} years old. You are interacting with the user to teach them about your culture. Be friendly and open, using the provided context as information about your culture.
         Question: {question}
         Context: {context}
@@ -48,10 +53,46 @@ class Person:
 
     def respond(self, user_input: str) -> str:
         chain = (
-        {"question": RunnablePassthrough(), "name": lambda x: self._name, "country": lambda x: self._country, "context": lambda x: "blank", "age": lambda x: "25"}
+        {"question": RunnablePassthrough(), "name": lambda x: self._name, "country": lambda x: self._country, "context": self._retriever, "age": lambda x: "25"}
         | self._prompt
         | self._llm
         | StrOutputParser()
         )
 
         return chain.invoke(user_input)
+    
+def get_context(country: str):
+    url = "https://en.wikipedia.org/wiki/Star_Wars"
+
+    headers_to_split_on = [
+        ("h1", "Header 1"),
+        ("h2", "Header 2"),
+        ("h3", "Header 3"),
+        ("h4", "Header 4"),
+        ("div", "Divider")
+    ]
+
+    html_splitter = HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+
+    # for local file use html_splitter.split_text_from_file(<path_to_file>)
+    html_header_splits = html_splitter.split_text_from_url(url)
+    chunk_size = 1024
+    chunk_overlap = 128
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
+
+    # Split
+    splits = text_splitter.split_documents(html_header_splits)
+
+    embeddings = OctoAIEmbeddings()
+
+    vector_store = FAISS.from_documents(
+    splits,
+    embedding=embeddings
+)
+
+    retriever = vector_store.as_retriever()
+
+    return retriever
